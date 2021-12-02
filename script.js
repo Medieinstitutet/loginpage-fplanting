cdddf157a1be1691853506fa104d5ec9e3b00d85
fplanting <frida.planting@hotmail.com>
2021-12-02 13:05:31 +0100

    get elements in js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("inlogg");
+
+// hämta alla element
+
+const logIn = document.getElementById("logIn");
+const wrongInlog = document.getElementById("wrongInlog");
+const welcome = document.getElementById("welcome");
+
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+const usernameInput = document.getElementById("usernameInput");
+const passwordInput = document.getElementById("passwordInput");
+
+const button = document.getElementById("password");
+
+// hämtar list från localstorage & if/else om den inte finns = tom
+// json för att göra om sträng till en array
