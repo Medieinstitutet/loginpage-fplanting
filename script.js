a5068740de0b55778c46d3f33e16fbfae2f4f663
fplanting <frida.planting@hotmail.com>
2021-12-03 12:46:06 +0100

    added button

--- script.js
+++ script.js
@@ -12,7 +12,26 @@ const password = document.getElementById("password");
 const usernameInput = document.getElementById("usernameInput");
 const passwordInput = document.getElementById("passwordInput");
 
-const button = document.getElementById("password");
+const buttonLogin = document.getElementById("buttonLogin");
+const buttonLogout = document.getElementById("buttonLogout");
 
 // hämtar list från localstorage & if/else om den inte finns = tom
 // json för att göra om sträng till en array
+
+function getPostsFromLS() {
+    let collectedPosts = localStorage.getItem("posts")
+
+    let posts = []
+
+    if(collectedPosts) {
+        posts = JSON.parse(collectedPosts)
+    } 
+
+    return posts
+}
+
+// skapa function för button = inlogg
+
+// skapa function för byte av vilket state man är i, inloggad eller ej
+
+// function för om man skriver fel
