71adf0172b0c54d133146c3043e2e7103a2b189c
fplanting <frida.planting@hotmail.com>
2021-12-03 16:52:48 +0100

    added function for buttons

--- script.js
+++ script.js
@@ -1,64 +1,86 @@
 console.log("inlogg");
 
-// hämta alla element
-
-const inlogText = document.getElementById("inlogText");
-const wrongInlogText = document.getElementById("wrongInlogText");
-const welcomeText = document.getElementById("welcomeText");
-
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-
-//const usernameInput = document.getElementById("usernameInput");
-//const passwordInput = document.getElementById("passwordInput");
-
-const buttonLogin = document.getElementById("buttonLogin");
-
-//printar ut de nya divs genom localstorage
-// min array/function printList();
-
-// hämtar list från localstorage & if/else om den inte finns = tom
-// json för att göra om sträng till en array
-
-function getInlogFromLS() {
-    let collectedInlog = localStorage.getItem("inlog")
-
-    let inlog = []
-
-    if(collectedInlog) {
-        inlog = JSON.parse(collectedinlog)
-    } 
-
-    return inlog
-}
-
-// hämtar input, hämtar inlog från Localstorage, lägger till input i inlog och sparar i LS
-// json för att göra en array till en sträng 
-
-
-
-// skapa function för button = inlogg
+// döljer dessa knappar, ska bara synas vid vissa tillfällen
+document.getElementById("buttonLogout").style.display = "none";
+document.getElementById("buttonGetback").style.display = "none";
 
 buttonLogin.addEventListener("click", () => {
-
+    
     let usernameInput = document.getElementById("usernameInput").value
     let passwordInput = document.getElementById("passwordInput").value
 
     let username = "janne"
     let password = "test"
 
+
+    // rätt lösenord & användarnamn = döljer och visar vissa element
     if(usernameInput == username && passwordInput == password) {
-        document.getElementById("buttonLogin").innerHTML = "logga ut";
+        document.getElementById("buttonLogout").style.display = "block";
         document.getElementById("welcomeText").innerHTML = "Välkommen, du är inloggad!";
+
+        document.getElementById("username").style.display = "none";
+        document.getElementById("password").style.display = "none";
+        document.getElementById("inlogText").style.display = "none";
+        document.getElementById("usernameInput").style.display = "none";
+        document.getElementById("passwordInput").style.display = "none";
+        document.getElementById("buttonLogin").style.display = "none";
+        document.getElementById("buttonGetback").style.display = "none";
         console.group("inloggad")
+
+    // fel användarnamn & lösenord = döljer och visar valda element
     } else 
     {
+        document.getElementById("buttonGetback").style.display = "block";
         document.getElementById("wrongInlogText").innerHTML = "fel lösenord eller användarnamn";
+
+        document.getElementById("username").style.display = "none";
+        document.getElementById("password").style.display = "none";
+        document.getElementById("inlogText").style.display = "none";
+        document.getElementById("usernameInput").style.display = "none";
+        document.getElementById("passwordInput").style.display = "none";
+        document.getElementById("buttonLogin").style.display = "none";
+        document.getElementById("buttonLogout").style.display = "none";
+
     console.log("fel lösenord")
+
     }
+
     console.log(buttonLogin)
 })
 
-// skapa function för byte av vilket state man är i, inloggad eller ej
 
-// function för om man skriver fel
+// En funktion för min logga-ut-knapp, tar fram element
+
+buttonLogout.addEventListener("click", () => {
+
+    document.getElementById("usernameInput").style.display = "block";
+    document.getElementById("passwordInput").style.display = "block";
+    document.getElementById("username").style.display = "block";
+    document.getElementById("password").style.display = "block";
+    document.getElementById("buttonLogin").style.display = "block";
+    document.getElementById("inlogText").style.display = "block";
+
+    document.getElementById("welcomeText").style.display = "none";
+    document.getElementById("buttonLogout").style.display = "none";
+    document.getElementById("buttonGetback").style.display = "none";
+    console.log(buttonLogout)
+})
+
+// En funktion för min gå-tillbaka-knapp, tar fram element
+
+buttonGetback.addEventListener("click", () => {
+
+    document.getElementById("usernameInput").style.display = "block";
+    document.getElementById("passwordInput").style.display = "block";
+    document.getElementById("username").style.display = "block";
+    document.getElementById("password").style.display = "block";
+    document.getElementById("buttonLogin").style.display = "block";
+    document.getElementById("inlogText").style.display = "block";
+
+    document.getElementById("welcomeText").style.display = "none";
+    document.getElementById("buttonLogout").style.display = "none";
+    document.getElementById("wrongInlogText").style.display = "none";
+    document.getElementById("buttonGetback").style.display = "none";
+
+    console.log(buttonGetback)
+})
