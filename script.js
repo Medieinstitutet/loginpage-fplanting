a589518807a20c3631e2110e64fd2b466d41d51a
fplanting <frida.planting@hotmail.com>
2021-12-03 14:39:45 +0100

    added function for button

--- script.js
+++ script.js
@@ -2,35 +2,60 @@ console.log("inlogg");
 
 // hämta alla element
 
-const logIn = document.getElementById("logIn");
-const wrongInlog = document.getElementById("wrongInlog");
-const welcome = document.getElementById("welcome");
+const inlogText = document.getElementById("inlogText");
+const wrongInlogText = document.getElementById("wrongInlogText");
+const welcomeText = document.getElementById("welcomeText");
 
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 
-const usernameInput = document.getElementById("usernameInput");
-const passwordInput = document.getElementById("passwordInput");
+//const usernameInput = document.getElementById("usernameInput");
+//const passwordInput = document.getElementById("passwordInput");
 
 const buttonLogin = document.getElementById("buttonLogin");
 const buttonLogout = document.getElementById("buttonLogout");
 
+//printar ut de nya divs genom localstorage
+// min array/function printList();
+
 // hämtar list från localstorage & if/else om den inte finns = tom
 // json för att göra om sträng till en array
 
-function getPostsFromLS() {
-    let collectedPosts = localStorage.getItem("posts")
+function getInlogFromLS() {
+    let collectedInlog = localStorage.getItem("inlog")
 
-    let posts = []
+    let inlog = []
 
-    if(collectedPosts) {
-        posts = JSON.parse(collectedPosts)
+    if(collectedInlog) {
+        inlog = JSON.parse(collectedinlog)
     } 
 
-    return posts
+    return inlog
 }
 
+// hämtar input, hämtar inlog från Localstorage, lägger till input i inlog och sparar i LS
+// json för att göra en array till en sträng 
+
+
+
 // skapa function för button = inlogg
+
+buttonLogin.addEventListener("click", () => {
+
+    let usernameInput = document.getElementById("usernameInput").value
+    let passwordInput = document.getElementById("passwordInput").value
+
+    let username = "janne"
+    let password = "test"
+
+    if(usernameInput == username && passwordInput == password) {
+        document.getElementById("welcomeText").innerHTML = "Välkommen, du är inloggad";
+    } else 
+    {
+        document.getElementById("wrongInlogText").innerHTML = "fel lösenord eller användarnamn";
+    }
+    console.log(buttonLogin)
+})
 
 // skapa function för byte av vilket state man är i, inloggad eller ej
 
