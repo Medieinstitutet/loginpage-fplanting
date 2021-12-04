06aa6d21a4063a30bb07bdeab244e3875dea674a
fplanting <frida.planting@hotmail.com>
2021-12-04 13:19:39 +0100

    function loginbutton

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// hämtar in knapparna
+let buttonLogin = document.getElementById("buttonLogin");
+let buttonLogout = document.getElementById("buttonLogout");
+let buttonGetback = document.getElementById("buttonGetback");
+
+// funktion för min inlogg-knapp
+buttonLogin.addEventListener("click", checkInlogSaveToLS)
+
 
 // rätt eller fel inlogg sparas till LocalStorage
-
 function checkInlogSaveToLS () {
 
     let usernameInput = document.getElementById("usernameInput").value
@@ -13,94 +20,124 @@ function checkInlogSaveToLS () {
         let userObject = {username: username, password: password};
 
         localStorage.setItem("user", JSON.stringify(userObject));
+
+    // uppdaterar vyn för fel inlogg
     } else {
+        document.getElementById("viewInlogged").style.display = "none";
+        document.getElementById("viewWronginlog").style.display = "block";
+        document.getElementById("viewLogin").style.display = "none";
         console.log("fel användarnamn eller lösenord");
+    }
+}
+
+// function för om vi är inloggad eller inte och uppdaterar sidan utifrån det
+function updateView () {
+    let userString = localStorage.getItem("user")
+
+    // vyn om man är inloggad
+    if(userString) {
+        document.getElementById("viewInlogged").style.display = "block";
+        document.getElementById("viewWronginlog").style.display = "none";
+        document.getElementById("viewLogin").style.display = "none";
+
+        // lägger till en hälsning med användarnamnet
+        let userObject = JSON.parse(userString)
+        document.getElementById("welcomeText").innerHTML = "Välkommen, " + (userObject.username) + "!";
+
+
+    // vyn om man inte är inloggad
+    } else
+    {
+        document.getElementById("viewInlogged").style.display = "none";
+        document.getElementById("viewWronginlog").style.display = "none";
+        document.getElementById("viewLogin").style.display = "block";
     }
 }
 
 
 
+
 // döljer dessa knappar, ska bara synas vid vissa tillfällen
-document.getElementById("buttonLogout").style.display = "none";
-document.getElementById("buttonGetback").style.display = "none";
+// document.getElementById("buttonLogout").style.display = "none";
+// document.getElementById("buttonGetback").style.display = "none";
 
-buttonLogin.addEventListener("click", () => {
+// buttonLogin.addEventListener("click", () => {
     
-    let usernameInput = document.getElementById("usernameInput").value
-    let passwordInput = document.getElementById("passwordInput").value
+//     let usernameInput = document.getElementById("usernameInput").value
+//     let passwordInput = document.getElementById("passwordInput").value
 
-    let username = "janne"
-    let password = "test"
-
-
-    // rätt lösenord & användarnamn = döljer och visar vissa element
-    if(usernameInput == username && passwordInput == password) {
-        document.getElementById("buttonLogout").style.display = "block";
-        document.getElementById("welcomeText").innerHTML = "Välkommen, du är inloggad!";
-
-        document.getElementById("username").style.display = "none";
-        document.getElementById("password").style.display = "none";
-        document.getElementById("inlogText").style.display = "none";
-        document.getElementById("usernameInput").style.display = "none";
-        document.getElementById("passwordInput").style.display = "none";
-        document.getElementById("buttonLogin").style.display = "none";
-        document.getElementById("buttonGetback").style.display = "none";
-        console.group("inloggad")
-
-    // fel användarnamn & lösenord = döljer och visar valda element
-    } else 
-    {
-        document.getElementById("buttonGetback").style.display = "block";
-        document.getElementById("wrongInlogText").innerHTML = "fel lösenord eller användarnamn";
-
-        document.getElementById("username").style.display = "none";
-        document.getElementById("password").style.display = "none";
-        document.getElementById("inlogText").style.display = "none";
-        document.getElementById("usernameInput").style.display = "none";
-        document.getElementById("passwordInput").style.display = "none";
-        document.getElementById("buttonLogin").style.display = "none";
-        document.getElementById("buttonLogout").style.display = "none";
-
-    console.log("fel lösenord")
-
-    }
-
-    console.log(buttonLogin)
-})
+//     let username = "janne"
+//     let password = "test"
 
 
-// En funktion för min logga-ut-knapp, tar fram element
+//     // rätt lösenord & användarnamn = döljer och visar vissa element
+//     if(usernameInput == username && passwordInput == password) {
+//         document.getElementById("buttonLogout").style.display = "block";
+//         document.getElementById("welcomeText").innerHTML = "Välkommen, du är inloggad!";
 
-buttonLogout.addEventListener("click", () => {
+//         document.getElementById("username").style.display = "none";
+//         document.getElementById("password").style.display = "none";
+//         document.getElementById("inlogText").style.display = "none";
+//         document.getElementById("usernameInput").style.display = "none";
+//         document.getElementById("passwordInput").style.display = "none";
+//         document.getElementById("buttonLogin").style.display = "none";
+//         document.getElementById("buttonGetback").style.display = "none";
+//         console.group("inloggad")
 
-    document.getElementById("usernameInput").style.display = "block";
-    document.getElementById("passwordInput").style.display = "block";
-    document.getElementById("username").style.display = "block";
-    document.getElementById("password").style.display = "block";
-    document.getElementById("buttonLogin").style.display = "block";
-    document.getElementById("inlogText").style.display = "block";
+//     // fel användarnamn & lösenord = döljer och visar valda element
+//     } else 
+//     {
+//         document.getElementById("buttonGetback").style.display = "block";
+//         document.getElementById("wrongInlogText").innerHTML = "fel lösenord eller användarnamn";
 
-    document.getElementById("welcomeText").style.display = "none";
-    document.getElementById("buttonLogout").style.display = "none";
-    document.getElementById("buttonGetback").style.display = "none";
-    console.log(buttonLogout)
-})
+//         document.getElementById("username").style.display = "none";
+//         document.getElementById("password").style.display = "none";
+//         document.getElementById("inlogText").style.display = "none";
+//         document.getElementById("usernameInput").style.display = "none";
+//         document.getElementById("passwordInput").style.display = "none";
+//         document.getElementById("buttonLogin").style.display = "none";
+//         document.getElementById("buttonLogout").style.display = "none";
 
-// En funktion för min gå-tillbaka-knapp, tar fram element
+//     console.log("fel lösenord")
 
-buttonGetback.addEventListener("click", () => {
+//     }
 
-    document.getElementById("usernameInput").style.display = "block";
-    document.getElementById("passwordInput").style.display = "block";
-    document.getElementById("username").style.display = "block";
-    document.getElementById("password").style.display = "block";
-    document.getElementById("buttonLogin").style.display = "block";
-    document.getElementById("inlogText").style.display = "block";
+//     console.log(buttonLogin)
+// })
 
-    document.getElementById("welcomeText").style.display = "none";
-    document.getElementById("buttonLogout").style.display = "none";
-    document.getElementById("wrongInlogText").style.display = "none";
-    document.getElementById("buttonGetback").style.display = "none";
 
-    console.log(buttonGetback)
-})
+// // En funktion för min logga-ut-knapp, tar fram element
+
+// buttonLogout.addEventListener("click", () => {
+
+//     document.getElementById("usernameInput").style.display = "block";
+//     document.getElementById("passwordInput").style.display = "block";
+//     document.getElementById("username").style.display = "block";
+//     document.getElementById("password").style.display = "block";
+//     document.getElementById("buttonLogin").style.display = "block";
+//     document.getElementById("inlogText").style.display = "block";
+
+//     document.getElementById("welcomeText").style.display = "none";
+//     document.getElementById("buttonLogout").style.display = "none";
+//     document.getElementById("buttonGetback").style.display = "none";
+//     console.log(buttonLogout)
+// })
+
+// // En funktion för min gå-tillbaka-knapp, tar fram element
+
+// buttonGetback.addEventListener("click", () => {
+
+//     document.getElementById("usernameInput").style.display = "block";
+//     document.getElementById("passwordInput").style.display = "block";
+//     document.getElementById("username").style.display = "block";
+//     document.getElementById("password").style.display = "block";
+//     document.getElementById("buttonLogin").style.display = "block";
+//     document.getElementById("inlogText").style.display = "block";
+
+//     document.getElementById("welcomeText").style.display = "none";
+//     document.getElementById("buttonLogout").style.display = "none";
+//     document.getElementById("wrongInlogText").style.display = "none";
+//     document.getElementById("buttonGetback").style.display = "none";
+
+//     console.log(buttonGetback)
+// })
